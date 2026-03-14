0b9f9830b71d393fdfd28d5586df773c01c308e7
Michael Jones <michael.jones90@me.com>
2026-03-14 10:55:57 +0000

    Initial Commit

---
+++ HackingWithSwift-Flip/HackingWithSwift_FlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_FlipApp.swift
+//  HackingWithSwift-Flip
+//
+//  Created by Michael Jones on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_FlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
