6e0320bb6dea47981aaeb79065949b1dbf3e5cb4
Michael Jones <michael.jones90@me.com>
2026-03-14 10:59:54 +0000

    - initial commit. - 'StoneColour' enum to represent what each square contains.

---
+++ HackingWithSwift-Flip/Models/StoneColour.swift
@@ -0,0 +1,12 @@
+//
+//  StoneColour.swift
+//  HackingWithSwift-Flip
+//
+//  Created by Michael Jones on 14/03/2026.
+//
+
+import Foundation
+
+enum StoneColour: Int {
+    case empty, choice, white, black
+}
