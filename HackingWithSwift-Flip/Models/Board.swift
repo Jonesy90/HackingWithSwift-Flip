505c930b9336469aeebb9d1187cffd000cc0268a
Michael Jones <michael.jones90@me.com>
2026-03-14 12:04:00 +0000

    helper method to identify if the selection made by the player is valid

--- HackingWithSwift-Flip/Models/Board.swift
+++ HackingWithSwift-Flip/Models/Board.swift
@@ -25,4 +25,15 @@ class Board: NSObject {
         //default to play going first.
         currentPlayer = Player.allPlayers[0]
     }
+    
+    /// A helper method to check if the selection the player has made is in bounds.
+    /// - Parameters:
+    ///   - row: row selection made by user.
+    ///   - col: col selection made by user
+    /// - Returns: returns true or false if the selection made is valid.
+    func isInBounds(row: Int, col: Int) -> Bool {
+        row >= 0 && col >= 0 && row < Board.size && col < Board.size
+    }
+    
+    
 }
