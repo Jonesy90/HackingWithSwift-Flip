94a73822c2c69d749af3889530397f71dc686796
Michael Jones <michael.jones90@me.com>
2026-03-15 09:59:09 +0000

    flipping the stone animation added when a selection is made by the player

--- HackingWithSwift-Flip/Views/StoneView.swift
+++ HackingWithSwift-Flip/Views/StoneView.swift
@@ -8,18 +8,43 @@
 import SwiftUI
 
 struct StoneView: View {
-    var colour: StoneColour
+    var colour: StoneColour // the owner of the stone
+    
+    @State private var animationRotation = 0.0
+    @State private var displayedColour: StoneColour? // is the colour we're showing right now.
     
     var body: some View {
-        switch colour {
-        case .empty:
-            Color.clear
-        case .black:
-            Image(.black)
-        case .white:
-            Image(.white)
-        case .choice:
-            Image(.thinking)
+        Group {
+            switch displayedColour ?? colour {
+            case .black:
+                Image(.black)
+            case .white:
+                Image(.white)
+            case .choice:
+                Image(.thinking)
+            case .empty:
+                Color.clear
+            }
+        }
+        .scaleEffect(0.8)
+        .onAppear {
+            displayedColour = colour
+        }
+        .rotation3DEffect(.degrees(animationRotation), axis: (x: 1, y: 0, z: 0), perspective: 0)
+        .onChange(of: colour) { oldValue, newValue in
+            if oldValue == .empty {
+                displayedColour = newValue
+            } else {
+                withAnimation(.easeIn(duration: 0.2)) {
+                    animationRotation = 90
+                } completion: {
+                    displayedColour = newValue
+                    
+                    withAnimation(.easeOut(duration: 0.2)) {
+                        animationRotation = 0
+                    }
+                }
+            }
         }
     }
 }
